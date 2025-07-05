07c9679e23f990de5a4a0455db74b5f9e5d36bd0
SaraMsoso <Sara.soder44@gmail.com>
2025-07-06 00:00:35 +0200

    Set up application routing with paths for home, courses and schema pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  {
+    path: 'kurser',
+    loadComponent: () =>
+      import('./pages/course-page/course-page.component').then(m => m.CoursePageComponent)
+  },
+  {
+    path: 'ramschema',
+    loadComponent: () =>
+      import('./pages/schema-page/schema-page.component').then(m => m.SchemaPageComponent)
+  },
+  { path: '**', redirectTo: '' }
+];
